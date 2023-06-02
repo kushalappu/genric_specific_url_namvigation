868fc26181095c018f3092b6cf117ca335d5c5f0
kushalappu <kushal8701@gmail.com>
2023-06-02 10:45:33 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def akash(request):
+    return render(request,'akash.html')
+
+def dhiraj(request):
+    return render(request,'dhiraj.html')
